c2bdbeab610f6cafa94b176b4ab561fb3942cc98
Smajlovycc <86891976+Smajlovycc@users.noreply.github.com>
2021-08-08 02:31:08 +0200

    CustomEventHandler.cs
    The script

---
+++ CustomEventHandler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+public interface IOnKeyPressed
+{
+    void OnKeyPress(KeyCode key);
+}
+
+public class CustomEventHandler : MonoBehaviour, IOnKeyPressed
+{
+    private void Update()
+    {
+        HandleInput();
+    }
+    
+    void HandleInput()
+    {
+        if (hovering)
+        {
+            Array a = Enum.GetValues(typeof(KeyCode));
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Input.GetKeyDown((KeyCode)a.GetValue(i)))
+                {
+                    OnKeyPress((KeyCode)a.GetValue(i));
+                }
+            }
+        }
+    }
+    
+    public void OnKeyPress(KeyCode key)
+    {
+        // Your code goes here
+        print(key);
+    }
+}
